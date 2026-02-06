b1b6cbd36d7354e692d08d764a691379366dcc87
sanjeev singh <sanju682295@gmail.com>
2026-02-06 10:05:12 +0530

    Updates

--- _next/static/ZGBeoZFmAtM92PbyC1Mgd/_buildManifest.js
+++ _next/static/ZGBeoZFmAtM92PbyC1Mgd/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/summit-ai//_next/:path+",
+        "destination": "/summit-ai/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
